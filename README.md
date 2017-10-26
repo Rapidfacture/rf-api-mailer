b1120c912e87ef6d19faceb7784226979379e546
Jochen Hörmann <jochenh@jochenh-laptop>
2017-10-26 14:03:11 +0200

    Init API mailer service commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+# rf-api-mailer
+
+* unstable - do not use now
+* will be the common part for every rapidfacture app
+
+API mailer service.
+
+
+## Getting Started
+
+> npm install rf-api-mailer
+
+
+## Dependencies
+
+Needs to have simple-template-mailer & rf-api to be installed.
+
+
+## Legal Issues
+* License: MIT
+* Author: Rapidfacture GmbH
---
+++ index.js
@@ -0,0 +1,68 @@
+/* jshint node: true */ "use strict";
+
+/**
+ * API service: add a mailer
+ */
+
+module.exports.start = function(options, next) {
+
+   // get mail settings from db
+   db.global.settings
+      .findOne({
+         "name": "mail"
+      })
+      .exec(function(err, doc) {
+         if (doc && doc.settings) {
+            config.mail = doc.settings;
+            startMailer();
+         } else {
+            log.error("no mail settings found in DB global");
+         }
+      });
+
+
+      function startMailer() {
+         var mailer = simpleTemplateMailer({ // create instance
+            // from db
+            transporter: config.mail.transporter,
+            defaultLanguage: config.mail.defaultLanguage,
+            // paths: from config file
+            translationsPath: config.configPaths.mail + "/translations",
+            templatesPath: config.configPaths.mail + "/templates"
+         });
+
+
+         function sendMail(template, options, sucessFunction, errorFunction) {
+            options = options || {};
+            options.from = options.from || config.mail.contactMail;
+            options.replyTo = options.replyTo || config.mail.contactMail;
+
+            var self = this; // this will hold variables from the answer function
+
+            mailer.send(template, options, function(data, info) {
+               log.success("mail sent");
+               if (sucessFunction){
+                  sucessFunction(data, info);
+               }
+            }, function(err, info) {
+               if (errorFunction){
+                  log.error("mailer error ", err, info);
+                  errorFunction(err, info);
+               } else {
+
+                  // TODO better custom errors here
+
+                  self.error("Mailer error: " + err);
+                  return;
+               }
+            });
+         }
+
+         API.registerAnswerFunction(sendMail);
+
+         //console.log(API);
+
+         next();
+      }
+
+};
